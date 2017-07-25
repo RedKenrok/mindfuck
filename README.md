1df578d7bbad7c082de8dc7d929339f5d9453b39
Ron Dekker <RedKenrok@gmail.com>
2017-07-25 13:21:10 +0200

    Publish 0.0.0

---
+++ README.md
@@ -0,0 +1,72 @@
+# Mindfuck
+Mindfuck is a Brainfuck interperter usable as a [node.js](https://nodejs.org) module, a command-line tool, and as a web library.
+
+## Node.js module
+1. Install node.js and npm.js
+	* See the [installation guide](https://docs.npmjs.com/getting-started/installing-node)
+2. Run from your project folder
+```
+npm install mindfuck
+```
+2. Or add the following to your package.json
+```
+"dependencies": {
+	"mindfuck": "0.x"
+},
+```
+3. See the [Library section](#Library) for futher usage
+
+## Command line
+1. Install node.js and npm.js
+	* See the [installation guide](https://docs.npmjs.com/getting-started/installing-node)
+2. Run
+```
+npm install mindfuck -g
+```
+
+### Commands
+```
+Usage:                          fitp [options]
+Options:
+  -c "<code>", -code "<code>"   runs the given code
+  -e <bool>, -error <bool>      stop when encountering an error, default false
+  -h, -help                     command options
+  -m, -memory                   memory cell length, default 30000
+  -p <path>, -path <path>       runs using the the code from the file
+  -v, -version                  semantic version
+```
+
+## Web
+1. Copy the [brainfuck.js](https://github.com/RedKenrok/brainfuck.js/tree/master/libs/mindfuck) file from the [libs](https://github.com/RedKenrok/mindfuck/tree/master/libs) folder
+2. See the [Library section](#Library) for futher usage
+3. See the example in the [example folder](https://github.com/RedKenrok/brainfuck.js/tree/master/example/web)
+
+## Library
+### Functions
+```
+brainfuck
+    Parameter(s): object options
+	Parse options to the library, see options
+brainfuck.run
+	Parameter(s): string code
+	Runs the given code
+```
+
+### Options
+```
+memoryLength
+	Default: 30000
+	Amount of cells in memory
+returnOnError
+	Default: false
+	Stop when encountering an unknown operation
+callbackInput
+	Parameter(s): function callback
+	Called when input required, call the callback with the input in order to continue the program
+callbackOutput
+	Parameter(s): char value
+	Called when output operation encountered
+callbackComplete
+	Parameter(s): string error
+	Called when program is finished, or an error has been encountered if returnOnError is true
+```
---
+++ index.js
@@ -0,0 +1,111 @@
+#! /usr/bin/env node
+/*! Brainfuck.js, v0.0.0, https://github.com/RedKenrok/brainfucked-js, MIT License */
+
+// Setup node.js' module.
+let fileSystem = require('fs'),
+	path = require('path'),
+// Get the brainfuck interpreter from the libraries folder.
+	brainfuck = module.exports = require('./lib/brainfuck');
+
+// Add command function to the exports.
+brainfuck.command = function(args) {
+	args = require('minimist')(
+		args,
+		{
+			alias: {
+				c: 'code',
+				e: 'error',
+				h: 'help',
+				m: 'memory',
+				p: 'path',
+				v: 'version'
+			},
+			unknown: function() {
+				console.log('Unknown argument given, use -h for more information');
+			}
+		});
+	
+	// No arguments or help.
+	if (args.length <= 0 || args.hasOwnProperty('h')) {
+		console.log(
+			'Usage:                          fitp [options]\n'
+		  + 'Options:\n'
+		  + '  -c "<code>", -code "<code>"   runs the given code\n'
+		  + '  -e <bool>, -error <bool>      stop when encountering an error, default false\n'
+		  + '  -h, -help                     command options\n'
+		  + '  -m, -memory                   memory cell length, default 30000\n'
+		  + '  -p <path>, -path <path>       runs using the the code from the file\n'
+		  + '  -v, -version                  semantic version'
+			);
+	}
+	
+	// Version
+	if (args.hasOwnProperty('v')) {
+		console.log(
+			'Version:                        ' + require('./package.json').version
+			);
+	}
+	
+	// Set the brainfuck options
+	let options = {
+		callbackInput: function(callback) {
+			let input = function(data) {
+				process.stdin.removeListener('data', input)
+					.pause();
+				callback(data.substring(0, 1));
+			}
+			process.stdin.setEncoding('ascii')
+				.on('data', input)
+				.resume();
+		},
+		callbackOutput: function(value) {
+			process.stdout.write(value);
+		},
+		callbackComplete: function(error) {
+			if (error) {
+				console.error(error);
+			}
+		}
+	}
+	// Error
+	if (args.hasOwnProperty('e')) {
+		options.returnOnError = args.e;
+	}
+	// Memory
+	if (args.hasOwnProperty('m')) {
+		options.memoryLength = args.m;
+	}
+	
+	brainfuck(options);
+	
+	if (args.hasOwnProperty('c')) {
+		brainfuck.run(args.c);
+	}
+	
+	// Path
+	if (args.hasOwnProperty('p')) {
+		// Directory to build from exists.
+		if (!fileSystem.existsSync(args.p)) {
+			console.error('Invalid directory path, please make sure the file exists');
+			return;
+		}
+		
+		let buffer = '';
+		fileSystem.createReadStream(args.p)
+			.on(
+				'data',
+				function(data) {
+					buffer += data;
+				})
+			.on(
+				'end',
+				function(error) {
+					if (error) {
+						console.error(error);
+					}
+					brainfuck.run(buffer);
+				});
+	}
+}
+
+brainfuck.command(process.argv.slice(2));
